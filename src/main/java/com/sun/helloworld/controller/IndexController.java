69f1a50e9199dfe6bbe3117c4272da8df01b4ed7
sunhao <2080680175@qq.com>
2019-04-10 16:24:31 +0800

    配置文件

--- src/main/java/com/sun/helloworld/controller/IndexController.java
+++ src/main/java/com/sun/helloworld/controller/IndexController.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,31 +15,49 @@ import com.sun.helloworld.bean.User;
 @RestController
 @RequestMapping(value = "/index")
 public class IndexController {
+
+	@Value(value = "${ronconn.secret}")
+	private String secret;
 	
+	@Value(value = "${ronconn.number}")
+	private int number;
+	
+	@Value(value = "${ronconn.name}")
+	private String myName;
+	
+	@Value(value = "${ronconn.desc}")
+	private String myDesc;
+
 	@RequestMapping(value = "")
 	public String index() {
 		return "Hello World!";
 	}
-	
+
 	@RequestMapping(value = "get")
-	public Map<String, String> get(@RequestParam String name) {
-		Map<String, String> map = new HashMap<String, String>();
-		
+	public Map<String, Object> get(@RequestParam String name) {
+		Map<String, Object> map = new HashMap<String, Object>();
+
 		map.put("name", name);
+		map.put("secret", secret);
+		map.put("number", number);
+		map.put("myName", myName);
+		map.put("desc", myDesc);
 		map.put("value", "Hello World!");
+
+		System.out.println(new Date());
 		
 		return map;
 	}
-	
+
 	@RequestMapping(value = "find/{id}/{name}")
-	public User find(@PathVariable int id,String name) {
-		
+	public User find(@PathVariable int id, @PathVariable String name) {
+
 		User user = new User();
 		user.setId(id);
 		user.setName(name);
 		user.setDate(new Date());
-		
+
 		return user;
 	}
-	
+
 }
