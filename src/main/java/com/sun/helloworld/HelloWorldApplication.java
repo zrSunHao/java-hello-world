7d444d1f29383a953d7fb6772bf91e1f8488b667
sunhao <2080680175@qq.com>
2019-04-11 13:15:26 +0800

    自定义Servlet

--- src/main/java/com/sun/helloworld/HelloWorldApplication.java
+++ src/main/java/com/sun/helloworld/HelloWorldApplication.java
@@ -2,10 +2,19 @@ package com.sun.helloworld;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import com.sun.helloworld.util.servlet.CustomerServlet;
 
 @SpringBootApplication
 public class HelloWorldApplication {
 
+	@Bean
+	public ServletRegistrationBean<CustomerServlet> servletRegistrationBean() {
+		return new ServletRegistrationBean<CustomerServlet>(new CustomerServlet(),"/roncoo");
+	}
+	
 	//整个项目的入口
 	public static void main(String[] args) {
 		SpringApplication.run(HelloWorldApplication.class, args);
---
+++ src/main/java/com/sun/helloworld/util/servlet/CustomerServlet.java
@@ -0,0 +1,30 @@
+package com.sun.helloworld.util.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author x
+ *自定义 Servlet
+ */
+public class CustomerServlet extends HttpServlet{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Override
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		System.out.println("servelet get method!");
+		doPost(request, response);
+	}
+	
+	
+	@Override
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException,IOException{
+		System.out.println("servelet post method!");
+		response.getWriter().write("hello world,this is CustomerServlet!");
+	}
+}
